f20de205af8a62334e35e44a71d4d4b248f9c184
Francoparodi01 <francoparodi2001@gmail.com>
2023-11-12 12:40:37 -0300

    End & deply

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -28,12 +28,12 @@ const Detail = ({ exerciseDetail }) => {
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
       <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
         <Typography sx={{ fontSize: { lg: '64px', xs: '30px' } }} fontWeight={700} textTransform="capitalize">
-          `${name}`;
+          {name}
         </Typography>
         <Typography sx={{ fontSize: { lg: '24px', xs: '18px' } }} color="#4F4C4C">
           Exercises keep you strong.{' '}
           <span style={{ textTransform: 'capitalize' }}>{name}</span> bup is one
-          of the best <br /> exercises to target your `{target}` . It will help you improve your{' '}
+          of the best <br /> exercises to target your {target}. It will help you improve your{' '}
           <br /> mood and gain energy.
         </Typography>
         {extraDetail?.map((item) => (
